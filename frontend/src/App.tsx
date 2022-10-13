b3ecedf75924270247a5df4b4a11e41265a502f9
AbdulrasheedAdediran <abdrasheed1103@gmail.com>
2022-10-14 00:12:28 +0100

    bond interface

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,23 +2,32 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import { Dashboard, Bond, Stake, Trade, Error, Layout } from "pages";
+import { WagmiConfig, createClient } from "wagmi";
+import { getDefaultProvider } from "ethers";
 import "./App.css";
+
+const client = createClient({
+    autoConnect: true,
+    provider: getDefaultProvider(),
+});
 
 function App(): JSX.Element {
     return (
-        <AnimatePresence mode="wait">
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Layout />}>
-                        <Route index element={<Dashboard />} />
-                        <Route path="/bond" element={<Bond />} />
-                        <Route path="/stake" element={<Stake />} />
-                        <Route path="/trade" element={<Trade />} />
-                    </Route>
-                    <Route path="*" element={<Error />} />
-                </Routes>
-            </Router>
-        </AnimatePresence>
+        <WagmiConfig client={client}>
+            <AnimatePresence mode="wait">
+                <Router>
+                    <Routes>
+                        <Route path="/" element={<Layout />}>
+                            <Route index element={<Dashboard />} />
+                            <Route path="/bond" element={<Bond />} />
+                            <Route path="/stake" element={<Stake />} />
+                            <Route path="/trade" element={<Trade />} />
+                        </Route>
+                        <Route path="*" element={<Error />} />
+                    </Routes>
+                </Router>
+            </AnimatePresence>
+        </WagmiConfig>
     );
 }
 
--- frontend/src/components/account/Account.tsx
+++ frontend/src/components/account/Account.tsx
@@ -3,12 +3,11 @@ import React from "react";
 import { FaWallet } from "react-icons/fa";
 function Account(): JSX.Element {
     return (
-        <button className="flex-center-center border border-cerulean text-cerulean gap-x-2 px-2 py-2 rounded-[10px]">
-            {/* <span>0x00</span> */}
-            <button className="flex-center-between gap-x-2">
+        <button className="flex-center-center btn-no-fill">
+            <span className="flex-center-between gap-x-2">
                 <FaWallet className="inline" />
                 Connect Wallet
-            </button>
+            </span>
         </button>
     );
 }
