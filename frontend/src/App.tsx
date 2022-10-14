37e43cd0a8bca12089e96919be77000920e215da
EnebeliEmmanuel <iemmaenebeli@gmail.com>
2022-10-14 03:26:11 +0100

    [CHORE]-Connect-wallet-functionalities-done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,8 +6,7 @@ import { WagmiConfig, createClient } from "wagmi";
 import { getDefaultProvider } from "ethers";
 import "./App.css";
 import {
-    WagmiConfig,
-    createClient,
+   
     defaultChains,
     configureChains,
 } from "wagmi";
