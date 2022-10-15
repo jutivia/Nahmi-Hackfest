dedee84f293e6c1763d29d1a678a60e246049be7
Jumoke Boladale-Lawal <76624942+jutivia@users.noreply.github.com>
2022-10-15 15:31:46 +0100

    dashboard fix

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -54,7 +54,7 @@ function Dashboard() {
                     </div>
                     <ul className="grid h-[80%] content-center grid-flow-row grid-cols-3 gap-x-4">
                         <li className="dashboard-card">
-                            <h3 className="dashboard-card-header">Tokens</h3>
+                            <h3 className="dashboard-card-header text-left font-bold">Tokens</h3>
                             {tokens.map((token) => (
                                 <p key={token.symbol}>
                                     <span>{token.balance} </span>
@@ -63,7 +63,7 @@ function Dashboard() {
                             ))}
                         </li>
                         <li className="dashboard-card">
-                            <h3 className="dashboard-card-header">
+                            <h3 className="dashboard-card-header text-left font-bold">
                                 Bonded Assets
                             </h3>
                             <p>
@@ -72,7 +72,7 @@ function Dashboard() {
                             </p>
                         </li>
                         <li className="dashboard-card">
-                            <h3 className="dashboard-card-header">
+                            <h3 className="dashboard-card-header text-left font-bold">
                                 Staked Assets
                             </h3>
                             <p>
