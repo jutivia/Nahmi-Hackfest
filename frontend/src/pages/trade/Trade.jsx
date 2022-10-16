992e7c363391ac4f23dfa216b8003e3a600183d8
Jumoke Boladale-Lawal <76624942+jutivia@users.noreply.github.com>
2022-10-16 20:27:32 +0100

    burning tokens staked and UI improvements

--- frontend/src/pages/trade/Trade.jsx
+++ frontend/src/pages/trade/Trade.jsx
@@ -8,10 +8,12 @@ function Trade() {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             transition={{ duration: 0.5 }}
-            className="grid place-items-center"
+            className="grid place-items-center h-screen m-auto w-full"
         >
-            Trade
-        </motion.main>
+         <section className="page-content min-w-[50%] max-w-[30rem] text-white flex-center-center">
+                   <h2 className="font-bold text-white text-2xl"> Coming soon!</h2>
+                </section>
+                  </motion.main>
     );
 }
 
